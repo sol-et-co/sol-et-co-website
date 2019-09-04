fd5ed4b76543aee95bd5bf24f9c105b81ace354c
clairezed <clairezuliani@gmail.com>
2019-09-04 13:12:58 +0200

    Try adding home picture optimization

--- themes/sol-et-co/assets/js/base.js
+++ themes/sol-et-co/assets/js/base.js
@@ -4,6 +4,25 @@ AOS.init();
 
 document.addEventListener('DOMContentLoaded', function () {
 
+  // IMG LOADING
+
+  (() => {
+    'use strict';
+    // Page is loaded
+    const objects = document.getElementsByClassName('asyncImage'); Array.from(objects).map((item) => {
+      // Start loading image
+      const img = new Image();
+      img.src = item.dataset.src;
+      // Once image is loaded replace the src of the HTML element
+      img.onload = () => {
+        item.classList.remove('asyncImage');
+        return item.nodeName === 'IMG' ?
+          item.src = item.dataset.src :
+          item.style.backgroundImage = `url(${item.dataset.src})`;
+      };
+    });
+  })();
+
   // NAVBAR ==========================================================
   // Get all "navbar-burger" elements
   var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
